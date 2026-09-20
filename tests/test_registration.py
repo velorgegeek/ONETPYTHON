e7d6ba90dff3d0ff7466b876154da1aee2576f99
velorgegeek <medvedevee@mer.ci.nsu.ru>
2026-09-20 20:18:42 +0700

    oh yea

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -4,93 +4,93 @@ import src.registration_service as srvc
 class test_registration(unittest.TestCase):
 
     def test_valid_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "абвгдежН7_", "абвгдежН7_")
         self.assertTrue(valid)
 
     def test_registration_with_wrong_Login(self):
-        srvc.main()
+        
         valid, msg =  srvc.register("AZAZAуййуйуZAZAZ_8","абвгдежН7_","абвгдежН7_")
         self.assertFalse(valid)
     def test_registration_with_wrong_Password_repeat(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "абвгдежН7_", "абвгдежqq7_")
         self.assertFalse(valid)
 
     def test_registration_with_wrong_password_len(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "аВб7_", "аВб7_")
         self.assertFalse(valid)
     def test_registration_pass_without_special_symbol(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "абвгдежН7", "абвгдежН7")
         self.assertFalse(valid)
 
     def test_registration_pass_without_num_symbol(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "абвгдежН_", "абвгдежН_")
         self.assertFalse(valid)
 
     def test_registration_pass_non_cyrilic_symbol(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "аahujaujakaН_", "аahujaujakaН_")
         self.assertFalse(valid)
 
     def test_registration_pass_upper_case(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "абвгдежН7_".upper(), "абвгдежН7_".upper())
         self.assertFalse(valid)
 
     def test_registration_pass_lower_case(self):
-        srvc.main()
+        
         valid, msg = srvc.register("AZAZAZAZAZ_8", "абвгдежН7_".lower(), "абвгдежН7_".lower())
         self.assertFalse(valid)
 
     def test_registration_login_in_blocked_list(self):
-        srvc.main()
+        
         valid, msg = srvc.register("redsusik228", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
     def test_registration_login_in_blocked_list2(self):
-        srvc.main()
+        
         valid, msg = srvc.register("adminboss42", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
     def test_registration_login_invalid_len(self):
-        srvc.main()
+        
         valid, msg = srvc.register("admi", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
     def test_valid_mail_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("example@da.com", "абвгдежН7_", "абвгдежН7_")
         self.assertTrue(valid)
     def test_valid_num_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("+7 777 777 77 77", "абвгдежН7_", "абвгдежН7_")
         self.assertTrue(valid)
     def test_invalid_num_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("+7 777 777 77999999 77", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
     def test_invalid_mail_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("exampleda.com", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
     def test_invalid_login_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("дадаадда8&", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
 
     def test_invalid_login_without_num_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("nmananan&", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
 
     def test_invalid_login_without_special_symbol_registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("nmananan8", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
 
     def test_invalid_login_without_special_symbol__registration(self):
-        srvc.main()
+        
         valid, msg = srvc.register("nmananan8&", "абвгдежН7_", "абвгдежН7_")
         self.assertFalse(valid)
 
